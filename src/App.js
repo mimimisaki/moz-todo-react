19cf4d7174cac63034bd012f68c65dafc60e2bde
mimimisaki <mimimisaki2@gmail.com>
2020-10-29 23:44:29 +0900

    [Finish] task completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import { nanoid } from "nanoid";
 
 export default function App(props) {
-  const taskList = props.tasks.map((task) => (
+  const [tasks, setTasks] = useState(props.tasks);
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
-      autoComplete={task.autoComplete}
+      completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
     />
   ));
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const handingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception"></div>
       <FilterButton />
       <FilterButton />
       <FilterButton />
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{handingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
